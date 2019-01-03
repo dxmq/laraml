fe515e280a4e567103d9908524f678c9631c5017
h1ml <1697859639@qq.com>
2019-01-03 17:53:17 +0800

    后台文件上传

--- config/blog.php
+++ config/blog.php
@@ -1,5 +1,9 @@
 <?php
 return [
     'title' => 'My Blog',
-    'posts_per_page' => 5 //每页显示文章数
+    'posts_per_page' => 5, //每页显示文章数
+    'uploads' => [
+        'storage' => 'public',
+        'webpath' => '/storage'
+    ]
 ];
